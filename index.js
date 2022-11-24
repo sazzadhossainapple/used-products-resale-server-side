aed27ce539f8a889830c79bc72c259d72e5d6291
sazzadhossainapple <sazzadapple81@gmail.com>
2022-11-24 09:38:51 +0600

    all category added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -10,13 +11,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send({
-    status: "success",
-    message: "E-Shoppers Server is running.",
-  });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kqw4pwk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    // database collection
+    const productsCatagoreyCollection = client
+      .db("E-Shoppers")
+      .collection("productsCatagorey");
+
+    app.get("/", (req, res) => {
+      res.send({
+        status: "success",
+        message: "E-Shoppers Server is running.",
+      });
+
+      // get all catagory
+      app.get("/catagory", async (req, res) => {
+        const query = {};
+        const catagory = await productsCatagoreyCollection
+          .find(query)
+          .toArray();
+        res.send(catagory);
+      });
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
 app.listen(port, () => {
   console.log(`E-Shoppers server on running port ${port}`);
 });
